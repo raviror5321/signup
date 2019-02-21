f4b22c7d5a244a8f06b5d9e24246574489f36919
ravikrachha <44354830+ravikrachha@users.noreply.github.com>
2019-02-20 21:38:37 -0700

    app

---
+++ workspace/app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    # add custom create logic here
+    super
+  end
+
+  def update
+    super
+  end
+  
+  private
+  
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :dob, :avatar, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :dob, :avatar, :email, :password, :password_confirmation, :current_password)
+  end
+end 
---
+++ workspace/app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }, presence: true
+  validates :first_name, :last_name, :dob, presence: true
+  validate :future_dob
+  
+  def future_dob
+    if dob && dob >= Date.today
+      errors.add(:dob, "Can't be future Date")
+    end
+  end
+end
